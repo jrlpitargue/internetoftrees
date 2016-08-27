44f247dae02d635d8d2c96f8de2b651cf7409a14
Simon Barrameda <barramedasimon321@gmail.com>
2016-08-28 06:03:29 +0800

    Added dynamic adding of tags on load

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -17,25 +17,40 @@ function initialize_map() {
                 let coords = new google.maps.InfoWindow({
                     content:"<b>Lattitude:</b> " + val.lat + "<br /><b>Longitude:</b> " + val.lng
                 });
+                let tags = '';
+                if(val.status != null) {
+                    $.each(val.status, function(key, state) {
+                        if(key == 'endangered') {
+                            tags = tags + '<span class="new badge red" data-badge-caption="endangered"></span>';
+                        } else {
+                            tags = tags + '<span class="new badge purple darken-2" data-badge-caption="invasive"></span>';
+                        }
+                    });
+                }
+
                 marker.setMap(map);
                 google.maps.event.addListener(marker,'click',function() {
                     coords.open(map, marker);
                     template = [
-                        '<li class="collection-header row">',
-                            '<div class="col s10">',
-                                '<h4>', val.name, '</h4>',
-                                '<span class="new badge red" data-badge-caption="endangered"></span>',
-                                '<span class="new badge purple darken-2" data-badge-caption="invasive"></span>',
-                                '<span href="#" class="new badge grey darken-2" data-badge-caption="+"></span>',
-                            '</div>',
-                            '<a class="wiki" href="',val.wiki,'"><i class="material-icons">keyboard_arrow_right</i></a>',
-                        '</li>',
-                        '<li class="collection-item">Family: ',val.familyName,'</li>',
-                        '<li class="collection-item">Sea Level: ',val.seaLevel,'</li>'
+                        '<ul class="collection with-header" >',
+                            '<li class="collection-header row">',
+                                '<div class="col s10">',
+                                    '<h4>', val.name, '</h4>',
+                                    tags,
+                                    '<span href="#" class="new badge grey darken-2" data-badge-caption="+"></span>',
+                                '</div>',
+                                '<a class="wiki" href="',val.wiki,'"><i class="material-icons">keyboard_arrow_right</i></a>',
+                            '</li>',
+                            '<li class="collection-item">Family: ',val.familyName,'</li>',
+                            '<li class="collection-item">Sea Level: ',val.seaLevel,'</li>',
+                        '</ul>',
+                        '<div id="about">',
+                            '<i>Forester\'s Notes</i><br />',
+                            val.notes,
+                        '</div>'
                     ].join('');
-                    $('.detail-container .collection.with-header').empty();
-                    $('.detail-container .collection.with-header').append(template);
-                    $(treeChart(val.name))
+                    $('.detail-container').empty();
+                    $('.detail-container').append(template);
                 });
             });
         },
