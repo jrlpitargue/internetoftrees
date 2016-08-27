dbc9aeb6896c8cf28115102b02276f9420dbc36e
PJHRobles <joshuahrobles@gmail.com>
2016-08-28 06:36:23 +0800

    updated backend for image classification

--- backend/controllers/image.js
+++ backend/controllers/image.js
@@ -6,6 +6,7 @@ import fs        from 'fs';
 
 import config    from './../config/config';
 import util      from './../helpers/util';
+import Firebase  from './../lib/init';
 
 exports.get_tree_type = (req, res, next) => {
 
@@ -17,19 +18,41 @@ exports.get_tree_type = (req, res, next) => {
         );
         let output = '';
         let response;
-        console.log(req.file);
+        let type = '';
         if (typeof req.file === 'undefined') {
             return res.status(400).send({ MESSAGE: 'No image sent!' });
         }
+
         python.stdout.on('data', (data) => {
             output += data;
             output = output.split('\n')[1];
+            type = output.substr(0, output.length-9);
 
             response = {
-                type: output.substr(0, output.length-9),
+                type: type,
                 value: output.substr(-7,5),
                 match: parseFloat(output.substr(-7,5), 10) > 0.95
             }
+
+            const ref = Firebase.database().ref('trees');
+
+            ref.on('value', (data) => {
+                let trees = data.val();
+
+                for (let key in trees) {
+                    let tree = trees[key];
+
+                    if (tree.name.toLowerCase() === type.toLowerCase()) {
+                        response['familyName'] = tree.familyName;
+                        response['notes'] = tree.notes;
+                        response['endangered'] = tree.status.endangered;
+                        response['invasive'] = tree.status.invasive;
+                        response['seaLevel'] = tree.seaLevel;
+                        response['wiki'] = tree.wiki;
+                        response['wildlife'] = tree.wildlife;
+                    }
+                }
+            });
         });
 
         python.on('close', (code) => { 
